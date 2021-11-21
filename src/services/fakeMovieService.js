85c904c58d980a11ccedec34bcdcd85c1f9692a7
Anirban <anirbanmukherjee602@gmail.com>
2021-11-22 02:46:13 +0530

    just Update

--- src/services/fakeMovieService.js
+++ src/services/fakeMovieService.js
@@ -2,68 +2,125 @@ import * as genresAPI from "./fakeGenreService";
 
 const movies = [
   {
-    _id: "5b21ca3eeb7f6fbccd471815",
+    _id: "1",
     title: "Terminator",
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
     numberInStock: 6,
-    dailyRentalRate: 2.5,
-    publishDate: "2018-01-03T19:04:28.809Z"
+    dailyRentalRate: 2,
+    publishDate: "2018-01-03T19:04:28.809Z",
+    like: false
   },
   {
-    _id: "5b21ca3eeb7f6fbccd471816",
+    _id: "2",
     title: "Die Hard",
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
     numberInStock: 5,
-    dailyRentalRate: 2.5
+    dailyRentalRate: 2,
+    like: false
   },
   {
-    _id: "5b21ca3eeb7f6fbccd471817",
+    _id: "3",
     title: "Get Out",
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 8,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3,
+    like: false
   },
   {
-    _id: "5b21ca3eeb7f6fbccd471819",
+    _id: "4",
     title: "Trip to Italy",
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3,
+    like: false
   },
   {
-    _id: "5b21ca3eeb7f6fbccd47181a",
+    _id: "5",
     title: "Airplane",
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3,
+    like: false
   },
   {
-    _id: "5b21ca3eeb7f6fbccd47181b",
+    _id: "6",
     title: "Wedding Crashers",
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "Comedy" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3,
+    like: false
   },
   {
-    _id: "5b21ca3eeb7f6fbccd47181e",
+    _id: "7",
     title: "Gone Girl",
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 7,
-    dailyRentalRate: 4.5
+    dailyRentalRate: 4,
+    like: false
   },
   {
-    _id: "5b21ca3eeb7f6fbccd47181f",
+    _id: "8",
     title: "The Sixth Sense",
     genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Thriller" },
     numberInStock: 4,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3,
+    like: false
   },
   {
-    _id: "5b21ca3eeb7f6fbccd471821",
+    _id: "9",
     title: "The Avengers",
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Action" },
     numberInStock: 7,
-    dailyRentalRate: 3.5
+    dailyRentalRate: 3,
+    like: false
+  },
+  {
+    _id: "10",
+    title: "End Game",
+    genre: { _id: "5b21ca3eeb7f6fbccd471819", name: "Action" },
+    numberInStock: 5,
+    dailyRentalRate: 4,
+    like: false
+  },
+  {
+    _id: "11",
+    title: "Man Of Steel",
+    genre: { _id: "5b21ca3eeb7f6fbccd471820", name: "Action" },
+    numberInStock: 7,
+    dailyRentalRate: 4,
+    like: false
+  },
+  {
+    _id: "12",
+    title: "Interstellar",
+    genre: { _id: "5b21ca3eeb7f6fbccd47182a", name: "Action" },
+    numberInStock: 7,
+    dailyRentalRate: 5,
+    like: false
+  },
+  {
+    _id: "13",
+    title: "Spider-Man: Far from Home",
+    genre: { _id: "5b21ca3eeb7f6fbccd47182b", name: "Action" },
+    numberInStock: 6,
+    dailyRentalRate: 3,
+    like: false
+  },
+  {
+    _id: "14",
+    title: "Ant-Man",
+    genre: { _id: "5b21ca3eeb7f6fbccd47182c", name: "Action" },
+    numberInStock: 2,
+    dailyRentalRate: 3,
+    like: false
+  },
+  {
+    _id: "15",
+    title: "Deepwater Horizon",
+    genre: { _id: "5b21ca3eeb7f6fbccd47182d", name: "Action" },
+    numberInStock: 3,
+    dailyRentalRate: 2,
+    like: false
   }
 ];
 
